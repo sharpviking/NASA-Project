ae33d61666620a5838e1fb51adb9ddeb3e7588d2
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-07 15:56:04 +0530

    7th April Commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const cors = require('cors');
@@ -11,8 +13,14 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+
+})
+
 
 
 module.exports = app;
