21ae743870cb50b768544ab3480a8f640f09adcb
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-07 16:09:17 +0530

    7th April commit

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const cors = require('cors');
 
+const morgan = require('morgan');
+
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
@@ -11,6 +13,8 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
