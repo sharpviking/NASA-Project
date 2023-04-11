3c9636d5e6d25a201d03f663d0b20f106032515b
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-11 15:18:23 +0530

    10th April commit 'jest' and 'supertest testing codes

--- server/src/server.js
+++ server/src/server.js
@@ -9,9 +9,11 @@ const { loadPlanetsData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
-async function startServer() {
 
+async function startServer() {
     await loadPlanetsData();
+
+
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`)
     });
