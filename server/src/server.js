4a75797fb9bd3e87a4aae26db2b7c07f12d6dea0
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-17 14:57:30 +0530

    17th April commit

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
 
@@ -8,9 +10,25 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://nasa-api:fzlp4QOQL4iYEnQh@nasacluster.r0u69vi.mongodb.net/?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        // useFindAndModify: false, => "no longer supported in mongoose"
+        // useCreateIndex: true,   => "no longer supported in mongoose"
+        useUnifiedTopology: true,
+    });
     await loadPlanetsData();
 
 
