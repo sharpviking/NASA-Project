4e428b54e5cf5fac85c632a55b48cf11d4721344
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-19 15:13:00 +0530

    19th April commit

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,6 @@
-// const launches = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
 
 const launches = new Map();
 
@@ -16,15 +18,36 @@ const launch = {
     success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
+
+// launches.set(launch.flightNumber, launch);
 
 function existsLaunchWithId(launchId) {
     return launches.has(launchId);
 }
 
 
-function getAllLaunches() {
-    return Array.from(launches.values());
+async function getAllLaunches() {
+    return await launchesDatabase
+        .find({}, { '_id': 0, '__v': 0 });
+}
+
+
+async function saveLaunch(launch) {
+    const planet = await planet.findOne({
+        keler_name: launch.target,
+    });
+
+    if (!planet) {
+        throw new Error('No matching planet found!');
+    }
+
+
+    await launchesDatabase.updateOne({
+        flightNumber: launch.flightNumber,
+    }, launch, {
+        upsert: true,
+    })
 }
 
 function addNewLaunch(launch) {
