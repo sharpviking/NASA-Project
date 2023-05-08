1b66d0505af627801884643c1e24d22ffe56e547
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-08 15:36:02 +0530

    final debug

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,8 @@ app.use(cors({
 app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', '..', "client", "build")));
 
 app.use('/v1', api);
 
